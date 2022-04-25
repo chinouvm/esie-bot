49597a1891d9d79a570854ea441d4decca8318d5
Chinou van Maris <chinou@esie.nl>
2022-04-25 18:58:57 +0200

    New key fix

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
+mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASEKEY" \
---output ./key.json key.json.gpg
+--output $HOME/secrets/key.json key.json.gpg
