468ccec8759d289ebd0ea3ed497c7ba48b291182
Chinou van Maris <chinou@esie.nl>
2022-04-25 19:09:08 +0200

    Try: Fixing Decryption

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,6 +2,7 @@
 
 # Decrypt the file
 echo "$PWD"
+echo "$HOME"
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASEKEY" \
