6b75b9f57a88abf4f5344c9cad782b23cf96c795
Chinou van Maris <chinou@esie.nl>
2022-04-25 19:06:37 +0200

    Try: Fixing decryption

--- .github/scripts/decrypt_env.sh
+++ .github/scripts/decrypt_env.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENVKEYS" \
---output $HOME/secrets/.env .env.gpg
+--output ./secrets/.env .env.gpg
--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASEKEY" \
---output $HOME/secrets/key.json key.json.gpg
+--output ./secrets/key.json key.json.gpg
