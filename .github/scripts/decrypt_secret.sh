b7a7ba750273b378dced7b92e7aedab215671615
Chinou van Maris <chinou@esie.nl>
2022-04-25 19:30:46 +0200

    Try: Fixing Decryption

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,6 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 cd secrets
+echo "$DKEY1"
 gpg --quiet --batch --yes --decrypt --passphrase="$DKEY2" \
 --output key.json key.json.gpg
