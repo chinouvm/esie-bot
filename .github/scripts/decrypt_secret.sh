309970ea161a0286be5c37662ec57a67f4d96606
Chinou van Maris <chinou@esie.nl>
2022-04-25 19:14:10 +0200

    Try: Fixing Dycription

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,6 +3,6 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-cd ..
+echo "$PWD"
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASEKEY" \
 --output /home/chinou/actions-runner/_work/esie-bot/esie-bot/secrets/key.json key.json.gpg
