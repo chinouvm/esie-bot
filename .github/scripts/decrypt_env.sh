870ebacde3f8c5eeef461b4e6dafe87de3a96b45
Chinou van Maris <chinou@esie.nl>
2022-04-25 18:54:37 +0200

    FIle location invalid fix?

--- .github/scripts/decrypt_env.sh
+++ .github/scripts/decrypt_env.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENVKEYS" \
---output $HOME/.env .env.gpg
+--output ./.env .env.gpg
--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASEKEY" \
---output $HOME/key.json key.json.gpg
+--output ./key.json key.json.gpg
