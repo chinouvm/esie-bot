8025390957b72de0e1e1b692e038e9d826f96bf4
Chinou van Maris <chinou@esie.nl>
2022-04-25 19:10:06 +0200

    try: fixing decryption

--- .github/scripts/decrypt_env.sh
+++ .github/scripts/decrypt_env.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-echo "$PWD"
-echo "$HOME"
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENVKEYS" \
---output ./secrets/.env .env.gpg
+--output /home/chinou/actions-runner/_work/esie-bot/esie-bot/secrets/.env .env.gpg
--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-echo "$PWD"
-echo "$HOME"
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASEKEY" \
---output ./secrets/key.json key.json.gpg
+--output /home/chinou/actions-runner/_work/esie-bot/esie-bot/secrets/key.json key.json.gpg
